ab607b899283463a761715ab17f7d4ae462e77d9
Carlos Tello <ingcarlostello@hotmail.com>
2024-01-31 16:12:37 -0500

    revert: deshacer cambios de prueba

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Hello</Button>
-      <h1>Hello</h1>
-      <h1>Hello2</h1>
-      <h2>Hello3</h2>
     </main>
   );
 }
