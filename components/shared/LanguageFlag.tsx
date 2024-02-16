9ed7fc1948ea7174bed2d6ecbf94becc0984add2
EmmanuelGaravito <egmantilla21@gmail.com>
2024-02-15 19:25:34 -0500

    initial commit

--- components/shared/LanguageFlag.tsx
+++ components/shared/LanguageFlag.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 
 // @Assets
-import usaFlag from "../../app/assets/language-flags/USA-flag.png";
+import usaFlag from "../../app/assets/language-flags/usa-flag.png";
 import spainFlag from "../../app/assets/language-flags/spain-flag.png";
 
 // @next-intl
@@ -12,25 +12,25 @@ import { useTranslations } from "next-intl";
 import { ELanguages } from "@/enums/Languages";
 
 const languageFlags = {
-    en: { flag: usaFlag, alt: "English" },
-    es: { flag: spainFlag, alt: "Spanish" },
+  en: { flag: usaFlag, alt: "English" },
+  es: { flag: spainFlag, alt: "Spanish" },
 };
 
 interface ILanguageFlagProps {
-    language: ELanguages;
+  language: ELanguages;
 }
 
 const LanguageFlag = ({ language }: ILanguageFlagProps) => {
-    const t = useTranslations("LanguageSelector");
-    const { flag, alt } = languageFlags[language]  || {};
-    return flag ? (
-        <div className="flex justify-around">
-            <span>{t("locale", { locale: language })}</span>
-            <span className="ml-4">
-                <Image alt={alt} src={flag} width={23} />
-            </span>
-        </div>
-    ) : null;
+  const t = useTranslations("LanguageSelector");
+  const { flag, alt } = languageFlags[language] || {};
+  return flag ? (
+    <div className="flex justify-around">
+      <span>{t("locale", { locale: language })}</span>
+      <span className="ml-4">
+        <Image alt={alt} src={flag} width={23} />
+      </span>
+    </div>
+  ) : null;
 };
 
 export default LanguageFlag;
