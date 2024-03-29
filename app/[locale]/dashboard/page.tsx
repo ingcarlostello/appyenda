3a5165983f8f2baa1112a636307324bae543c9e1
Carlos Tello <ingcarlostello@hotmail.com>
2024-03-29 17:32:06 -0500

    feat: create metrics components to dashboard

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -1,5 +1,44 @@
+// @Components
+import { MetricCard } from "@/components/shared/MetricCard";
+
+// @Lucide react icons
+import { CalendarCheck, CircleDollarSign, Users } from "lucide-react";
+
 const Dashboard = () => {
-    return <div className="bg-green-400 w-full">Dashboard Data</div>;
+    return (
+        <div className="pl-10 w-full">
+            <p className="text-2xl font-semibold mb-10">Dasboard</p>
+
+            <div className="grid grid-cols-4 gap-4 w-full pr-8">
+                <div>
+                    <MetricCard 
+                        icon={<CircleDollarSign size={35} />}
+                        iconColor="bg-green-400"
+                        title="Total Ventas"
+                        value="$45,700"
+                    />
+                </div>
+
+                <div>
+                    <MetricCard
+                        icon={<CalendarCheck size={35} />}
+                        iconColor="bg-blue-400"
+                        title="Reservas"
+                        value="87"
+
+                    />
+                </div>
+                <div>
+                    <MetricCard
+                        icon={<Users size={35} />}
+                        iconColor="bg-orange-400"
+                        title="Clientes"
+                        value="250"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Dashboard;
