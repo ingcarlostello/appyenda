b68a9a6d819e09792197e99c7f7b362bd678d9e6
EmmanuelGaravito <egmantilla21@gmail.com>
2024-03-29 23:55:02 -0500

    first commit

--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -1,12 +1,11 @@
 import { Client, Account, Databases, Storage } from "appwrite";
 
 export const appwriteConfig = {
-    projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
-    url: process.env.NEXT_PUBLIC_APPWRITE_URL,
-    databaseId: process.env.NEXT_PUBLIC_DATABASE_ID,
-    storageId: process.env.NEXT_PUBLIC_APPWRITE_STORAGE_ID,
-    userCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION_ID
-
+  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
+  url: process.env.NEXT_PUBLIC_APPWRITE_URL,
+  databaseId: process.env.NEXT_PUBLIC_DATABASE_ID,
+  storageId: process.env.NEXT_PUBLIC_APPWRITE_STORAGE_ID,
+  userCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION_ID,
 };
 
 export const client = new Client();
