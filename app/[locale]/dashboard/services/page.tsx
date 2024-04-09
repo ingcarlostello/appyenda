65a14c4f480eb2a84ad6d8486b091731c5ca29ea
Carlos Tello <48301281+ingcarlostello@users.noreply.github.com>
2024-04-08 20:55:31 -0500

    feat: create table to list the available services (#26)

--- app/[locale]/dashboard/services/page.tsx
+++ app/[locale]/dashboard/services/page.tsx
@@ -1,9 +1,30 @@
+// @Components
+import ListTable from "@/components/shared/listTable/ListTable";
 
+// @Shadcn
+import { Button } from "@/components/ui/button";
+
+// @Icons
+import { PlusCircleIcon } from "lucide-react";
 
 const Services = () => {
+
     return (
         <div>
-            Services Page
+            <p className="xs:text-2xl xs:font-bold md:text-4xl md:font-bold lg:text-4xl lg:font-bold">
+                Services
+            </p>
+            <div className="flex justify-end pr-4">
+                <Button className="h-8 gap-1" size="sm">
+                    <PlusCircleIcon className="h-3.5 w-3.5" />
+                    <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                        Add Service
+                    </span>
+                </Button>
+            </div>
+            <div className="mt-8">
+                <ListTable />
+            </div>
         </div>
     );
 };
---
+++ components/shared/listTable/ListTable.tsx
@@ -0,0 +1,53 @@
+// @Shadcn
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+
+// @Next-intl
+import { useTranslations } from "next-intl";
+
+
+
+const ListTable = () => {
+    const x = [1, 2, 3, 4, 5];
+    const t = useTranslations("ServicesPage");
+
+    return (
+        <div>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>{t("NAME")}</TableHead>
+                        <TableHead className="hidden md:table-cell">{t("DESCRIPTION")}</TableHead>
+                        <TableHead>{t("DURATION")}</TableHead>
+                        <TableHead>{t("PRICE")}</TableHead>
+                        <TableHead className="hidden md:table-cell">{t("CATEGORY")}</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {x.map((item) => (
+                        <TableRow>
+                            <TableCell className="font-medium">Cancha 1</TableCell>
+                            <TableCell className="w-6/12 hidden md:table-cell">
+                                Curabitur non velit vitae urna dignissim fermentum. Proin nec
+                                suscipit dui, eget volutpat justo.
+                            </TableCell>
+                            <TableCell>60 min</TableCell>
+                            <TableCell>$ 120.000</TableCell>
+                            <TableCell className="hidden md:table-cell">Deportes</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    );
+};
+
+export default ListTable;
