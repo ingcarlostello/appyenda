f817f9370a8814a8313198b1ae0876a5a845b397
EmmanuelGaravito <egmantilla21@gmail.com>
2024-04-19 11:42:59 -0500

    add portugal-flag.png to assets

--- i18n.ts
+++ i18n.ts
@@ -1,19 +1,19 @@
-import { notFound } from 'next/navigation';
-import { getRequestConfig } from 'next-intl/server';
+import { notFound } from "next/navigation";
+import { getRequestConfig } from "next-intl/server";
 
 // Can be imported from a shared config
-export const locales = ['en', 'es'];
+export const locales = ["en", "es", "pt"];
 
 export default getRequestConfig(async ({ locale }) => {
-    // Validate that the incoming `locale` parameter is valid
-    if (!locales.includes(locale as any)) notFound();
+	// Validate that the incoming `locale` parameter is valid
+	if (!locales.includes(locale as any)) notFound();
 
-    return {
-        messages: (
-            await (locale === 'en'
-              ? // When using Turbopack, this will enable HMR for `en`
-                import('./messages/en.json')
-              : import(`./messages/${locale}.json`))
-          ).default
-    };
+	return {
+		messages: (
+			await (locale === "en"
+				? // When using Turbopack, this will enable HMR for `en`
+				  import("./messages/en.json")
+				: import(`./messages/${locale}.json`))
+		).default,
+	};
 });
--- middleware.ts
+++ middleware.ts
@@ -5,90 +5,90 @@ import createMiddleware from "next-intl/middleware";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(request: NextRequest) {
-    const restrictedPaths = [
-        "/dashboard/profile",
-        "/dashboard/calendar",
-        "/dashboard/services",
-    ];
-    const authToken = request.cookies.get("login-user-cookie")?.value;
-    const initialSocialCookie = request.cookies.get(
-        "initial-social-cookie"
-    )?.value;
+	const restrictedPaths = [
+		"/dashboard/profile",
+		"/dashboard/calendar",
+		"/dashboard/services",
+	];
+	const authToken = request.cookies.get("login-user-cookie")?.value;
+	const initialSocialCookie = request.cookies.get(
+		"initial-social-cookie"
+	)?.value;
 
-    const path = request.nextUrl.pathname;
+	const path = request.nextUrl.pathname;
 
-    if (!authToken && !initialSocialCookie && path === "/dashboard") {
-        NextResponse.next().cookies.delete("login-user-cookie");
-        NextResponse.next().cookies.delete("initial-social-cookie");
-        NextResponse.next().cookies.delete("social-account-cookie");
-        const response = NextResponse.redirect(new URL("/login", request.url));
-        return response;
-    }
+	if (!authToken && !initialSocialCookie && path === "/dashboard") {
+		NextResponse.next().cookies.delete("login-user-cookie");
+		NextResponse.next().cookies.delete("initial-social-cookie");
+		NextResponse.next().cookies.delete("social-account-cookie");
+		const response = NextResponse.redirect(new URL("/login", request.url));
+		return response;
+	}
 
-    function redirectToLogin(request: NextRequest): NextResponse {
-        NextResponse.next().cookies.delete("login-user-cookie");
-        NextResponse.next().cookies.delete("initial-social-cookie");
-        NextResponse.next().cookies.delete("social-account-cookie");
-        return NextResponse.redirect(new URL("/login", request.url));
-    }
+	function redirectToLogin(request: NextRequest): NextResponse {
+		NextResponse.next().cookies.delete("login-user-cookie");
+		NextResponse.next().cookies.delete("initial-social-cookie");
+		NextResponse.next().cookies.delete("social-account-cookie");
+		return NextResponse.redirect(new URL("/login", request.url));
+	}
 
-    if (!authToken && !initialSocialCookie && restrictedPaths.includes(path)) {
-        return redirectToLogin(request);
-    }
+	if (!authToken && !initialSocialCookie && restrictedPaths.includes(path)) {
+		return redirectToLogin(request);
+	}
 
-    if (
-        request.cookies.has("initial-social-cookie") &&
-        request.nextUrl.pathname.startsWith("/login") &&
-        !authToken
-    ) {
-        const response = NextResponse.redirect(new URL("/dashboard", request.url));
-        return response;
-    }
+	if (
+		request.cookies.has("initial-social-cookie") &&
+		request.nextUrl.pathname.startsWith("/login") &&
+		!authToken
+	) {
+		const response = NextResponse.redirect(new URL("/dashboard", request.url));
+		return response;
+	}
 
-    if (
-        request.cookies.has("social-account-cookie") &&
-        request.nextUrl.pathname.startsWith("/register")
-    ) {
-        const response = NextResponse.redirect(new URL("/dashboard", request.url));
-        return response;
-    }
+	if (
+		request.cookies.has("social-account-cookie") &&
+		request.nextUrl.pathname.startsWith("/register")
+	) {
+		const response = NextResponse.redirect(new URL("/dashboard", request.url));
+		return response;
+	}
 
-    if (authToken && request.nextUrl.pathname.startsWith("/register")) {
-        const response = NextResponse.redirect(new URL("/dashboard", request.url));
-        return response;
-    }
+	if (authToken && request.nextUrl.pathname.startsWith("/register")) {
+		const response = NextResponse.redirect(new URL("/dashboard", request.url));
+		return response;
+	}
 
-    if (authToken && request.nextUrl.pathname.startsWith("/login")) {
-        const response = NextResponse.redirect(new URL("/dashboard", request.url));
-        return response;
-    }
+	if (authToken && request.nextUrl.pathname.startsWith("/login")) {
+		const response = NextResponse.redirect(new URL("/dashboard", request.url));
+		return response;
+	}
 
-    const handleI18nRouting = createMiddleware({
-        // A list of all locales that are supported
-        locales: ["en", "es"],
-        // Used when no locale matches
-        defaultLocale: "en",
-        localeDetection: true,
-        localePrefix: "never",
-    });
-    const response = handleI18nRouting(request);
-    return response;
+	const handleI18nRouting = createMiddleware({
+		// A list of all locales that are supported
+		locales: ["en", "es", "pt"],
+		// Used when no locale matches
+		defaultLocale: "en",
+		localeDetection: true,
+		localePrefix: "never",
+	});
+	const response = handleI18nRouting(request);
+	return response;
 }
 
 export const config = {
-    matcher: [
-        // This entry handles the root of the base
-        // path and should always be included
-        "/",
-        // Match all pathnames except for
-        // - … if they start with `/api`, `/_next` or `/_vercel`
-        // - … the ones containing a dot (e.g. `favicon.ico`)
-        "/((?!api|_next|_vercel|.*\\..*).*)",
-        // However, match all pathnames within `/users`, optionally with a locale prefix
-        "/([\\w-]+)?/users/(.+)",
+	matcher: [
+		// This entry handles the root of the base
+		// path and should always be included
+		"/",
+		// Match all pathnames except for
+		// - … if they start with `/api`, `/_next` or `/_vercel`
+		// - … the ones containing a dot (e.g. `favicon.ico`)
+		"/((?!api|_next|_vercel|.*\\..*).*)",
+		// However, match all pathnames within `/users`, optionally with a locale prefix
+		"/([\\w-]+)?/users/(.+)",
 
-        "/dashboard/:path*",
-        // "/login",
-        '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    ],
+		"/dashboard/:path*",
+		// "/login",
+		"/((?!api|_next/static|_next/image|favicon.ico).*)",
+	],
 };
