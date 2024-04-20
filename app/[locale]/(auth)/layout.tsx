1c3112eb3960df4b9f2ec58d51b769cb313fd36e
EmmanuelGaravito <egmantilla21@gmail.com>
2024-04-20 00:00:52 -0500

    delete hard coded white backgrounds and black fonts

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -5,7 +5,7 @@ type AuthLayoutProps = { children: React.ReactNode };
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
 	return (
-		<main className="bg-white md:bg-slate-50">
+		<main className=" ">
 			<>
 				<div className="pt-4 mr-6 flex justify-end">
 					<LanguageSelector />
--- components/shared/mobileSidebar/MobileSidebar.tsx
+++ components/shared/mobileSidebar/MobileSidebar.tsx
@@ -1,21 +1,25 @@
+// @Shadcn
 import { Button } from "@/components/ui/button";
 import { Nav } from "@/components/ui/nav";
 import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetFooter,
-  SheetTrigger,
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetFooter,
+	SheetTrigger,
 } from "@/components/ui/sheet";
 import MobileSidebarViewModel from "./MobileSidebarViewModel";
+import { Switch } from "@/components/ui/switch";
 
 // @Lucide-react
 import {
-  Briefcase,
-  CalendarDays,
-  LayoutDashboard,
-  Menu,
-  User,
+	Briefcase,
+	CalendarDays,
+	LayoutDashboard,
+	Menu,
+	User,
+	Sun,
+	Moon,
 } from "lucide-react";
 import { APPYENDA } from "@/constants/pages";
 
@@ -23,53 +27,60 @@ import { APPYENDA } from "@/constants/pages";
 import { useTranslations } from "next-intl";
 
 export function MobileSidebar() {
-  const { handleSignOut } = MobileSidebarViewModel();
+	const { handleSignOut } = MobileSidebarViewModel();
 
-  const t = useTranslations("Sidebar");
+	const t = useTranslations("Sidebar");
 
-  return (
-    <div>
-      <Sheet>
-        <SheetTrigger asChild>
-            <Menu size={38} />
-        </SheetTrigger>
-        <SheetContent side="left">
-          <Nav
-            isCollapsed={false}
-            links={[
-              {
-                title: `${t("DASHBOARD")}`,
-                href: "/dashboard",
-                icon: LayoutDashboard,
-                variant: "default",
-              },
-              {
-                title: `${t("CALENDAR")}`,
-                href: APPYENDA.CALENDAR,
-                icon: CalendarDays,
-                variant: "ghost",
-              },
-              {
-                title: `${t("SERVICES")}`,
-                href: APPYENDA.SERVICES,
-                icon: Briefcase,
-                variant: "ghost",
-              },
-              {
-                title: `${t("PROFILE")}`,
-                href: APPYENDA.PROFILE,
-                icon: User,
-                variant: "ghost",
-              },
-            ]}
-          />
-          <SheetFooter>
-            <SheetClose asChild>
-              <Button onClick={handleSignOut} type="submit">{t("LOGOUT")}</Button>
-            </SheetClose>
-          </SheetFooter>
-        </SheetContent>
-      </Sheet>
-    </div>
-  );
+	return (
+		<div>
+			<Sheet>
+				<SheetTrigger asChild>
+					<Menu size={38} />
+				</SheetTrigger>
+				<SheetContent side="left">
+					<Nav
+						isCollapsed={false}
+						links={[
+							{
+								title: `${t("DASHBOARD")}`,
+								href: "/dashboard",
+								icon: LayoutDashboard,
+								variant: "default",
+							},
+							{
+								title: `${t("CALENDAR")}`,
+								href: APPYENDA.CALENDAR,
+								icon: CalendarDays,
+								variant: "ghost",
+							},
+							{
+								title: `${t("SERVICES")}`,
+								href: APPYENDA.SERVICES,
+								icon: Briefcase,
+								variant: "ghost",
+							},
+							{
+								title: `${t("PROFILE")}`,
+								href: APPYENDA.PROFILE,
+								icon: User,
+								variant: "ghost",
+							},
+						]}
+					/>
+					<SheetFooter className="flex flex-col justify-between">
+						<SheetClose asChild>
+							<Button onClick={handleSignOut} type="submit">
+								{t("LOGOUT")}
+							</Button>
+						</SheetClose>
+						<div className="flex">
+							<Moon className=" w-4" />
+							<Switch className="relative" />
+							<Sun className=" w-4" />
+						</div>
+					</SheetFooter>
+				</SheetContent>
+			</Sheet>
+		</div>
+	);
 }
