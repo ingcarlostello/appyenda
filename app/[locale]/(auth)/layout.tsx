0cddae2e8a10e9fedc430fe45e448a52fd9cb4a5
EmmanuelGaravito <egmantilla21@gmail.com>
2024-04-20 10:56:49 -0500

    remove mode toggle from sidebar and add it to main nav bar in larger screens

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 // @Components
 import LanguageSelector from "@/components/shared/LanguageSelector";
+import { ModeToggle } from "@/components/theme-toggle";
 
 type AuthLayoutProps = { children: React.ReactNode };
 
@@ -7,7 +8,10 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
 	return (
 		<main className=" ">
 			<>
-				<div className="pt-4 mr-6 flex justify-end">
+				<div className="pt-4 mr-6 flex justify-end gap-2">
+					<div className="hidden sm:block">
+						<ModeToggle />
+					</div>
 					<LanguageSelector />
 				</div>
 				{children}
--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -15,16 +15,16 @@ const Layout = ({ children }: DashboardLayoutProps) => {
 				<MobileSidebar />
 			</div>
 
-			<div className="flex justify-end pr-4 pt-2">
+			<div className="flex justify-end  gap-2 pr-4 pt-2">
+				<div className="hidden sm:block">
+					<ModeToggle />
+				</div>
 				<LanguageSelector />
 			</div>
 
 			<div className="flex">
-				<div className=" relative w-2/12 border-r h-screen max-[767px]:hidden md:block md:w-1/4 lg:w-1/6">
+				<div className="  w-2/12 border-r h-screen max-[767px]:hidden md:block md:w-1/4 lg:w-1/6">
 					<Sidebar />
-					<div className="absolute bottom-3 left-2">
-						<ModeToggle />
-					</div>
 				</div>
 
 				<div className="w-full pl-8">{children}</div>
