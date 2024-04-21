44929aed9bc5fb915073bcd9f6689f45b83bb942
EmmanuelGaravito <egmantilla21@gmail.com>
2024-04-21 16:22:02 -0500

    fix: move language selector and sidebar upwards for overall consistency

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -10,18 +10,20 @@ type DashboardLayoutProps = { children: React.ReactNode };
 
 const Layout = ({ children }: DashboardLayoutProps) => {
 	return (
-		<div className="h-screen">
-			<div className="md:hidden pl-4 pt-4">
-				<MobileSidebar />
-			</div>
-			<div className="flex justify-end  gap-2 pr-4 pt-2">
-				<div className="hidden sm:block">
-					<ModeToggle />
+		<div className="h-screen ">
+			<div className="flex justify-between place-items-end sm:justify-end">
+				<div className="md:hidden pl-4 pt-4">
+					<MobileSidebar />
 				</div>
-				<LanguageSelector />
+				<div className="flex justify-end  gap-2 pr-4 pt-2">
+					<div className="hidden sm:block">
+						<ModeToggle />
+					</div>
+					<LanguageSelector />
+				</div>
 			</div>
-			<div className="flex">
-				<div className="  w-2/12 border-r h-screen max-[767px]:hidden md:block md:w-1/4 lg:w-1/6">
+			<div className=" flex">
+				<div className="  w-2/12 border-r h-screen sm:relative sm:-top-11  max-[767px]:hidden md:block md:w-1/4 lg:w-1/6">
 					<Sidebar />
 				</div>
 				<div className="w-full pl-8">{children}</div>
